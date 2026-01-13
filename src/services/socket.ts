3fae092a5dc9bdff5fd8214fbc5534ad43196f28
Lucas Calixto <lucas-calixtooo@hotmail.com>
2026-01-12 22:35:06 -0300

    Update socket.ts

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -20,7 +20,7 @@ import { type callType } from '../types/call'
 export function startSocket() {
   const { setCallsHistory, getThisCall, updateStatus } = CallsHistoryModel
 
-  const socket = io(`${process.env.SOCKET_URL}`, {
+  const socket = io('http://apimonitor.speakr.com.br:3002', {
     transports: ['websocket'],
     reconnection: true,
     reconnectionAttempts: 30,
